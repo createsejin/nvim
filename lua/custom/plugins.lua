8c11707f24af4d7da211169e5de10a9beb975155
createjin <createsejin@gmail.com>
2024-04-09 17:04:59 +0900

    2024-04-09-003 move auto-session configs to configs folder

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -200,28 +200,7 @@ local plugins = {
   {
     "rmagatti/auto-session",
     config = function()
-      require("auto-session").setup({
-        log_level = vim.log.levels.ERROR,
-        auto_restore_enabled = false,
-        auto_session_suppress_dirs = { "~/", "/home/bae", "~/Projects", "~/Downloads", "/" },
-        -- ⚠️ This will only work if Telescope.nvim is installed
-        -- The following are already the default values, no need to provide them if these are already the settings you want.
-        session_lens = {
-          -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-          buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-          load_on_setup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-        -- Set mapping for searching a session.
-        -- ⚠️ This will only work if Telescope.nvim is installed
-        vim.keymap.set("n", "<leader>cs",
-          require("auto-session.session-lens").search_session, {
-            noremap = true,
-            desc = "Search session"
-          }),
-        auto_session_enable_last_session = true,
-      })
+      require("custom.configs.auto-session")
     end,
     event = "BufWinEnter",
   },

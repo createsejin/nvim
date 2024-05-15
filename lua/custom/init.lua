cd96c9397e3ff8ab085896a1d2cdb3d2ab0180b8
createjin <createsejin@gmail.com>
2024-05-16 01:12:56 +0900

    windows11 nvim config for neovide set font size and shell

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,11 +2,11 @@ local opt = vim.opt
 opt.colorcolumn = "90"
 opt.guicursor =
 "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-opt.shell = "/usr/bin/zsh"
+opt.shell = "cmd"
 opt.relativenumber = true
 opt.encoding = "UTF-8"
 opt.scrolloff = 9
-opt.guifont = "Source Code Pro,SauceCodePro Nerd Font:h14"
+opt.guifont = "Source Code Pro,SauceCodePro Nerd Font:h13.23"
 opt.linespace = 0
 vim.g.neovide_scale_factor = 1.0
 vim.g.neovide_font_hinting = 'none'
